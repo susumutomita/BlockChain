6dbd46bd50f14991595fcc3c10ccdd3e01a968a8
susumutomita <oyster880@gmail.com>
2025-04-15 09:15:46 +0900

    remove debug log from util

--- references/chapter7/src/utils.zig
+++ references/chapter7/src/utils.zig
@@ -1,15 +1,5 @@
 const std = @import("std");
 
-pub const debug_logging = false;
-
-/// debugLog:
-/// デバッグログを出力するためのヘルパー関数です。
-/// ※ debug_logging が true の場合のみ std.debug.print を呼び出します。
-pub fn debugLog(comptime format: []const u8, args: anytype) void {
-    if (comptime debug_logging) {
-        std.debug.print(format, args);
-    }
-}
 
 /// truncateU32ToU8:
 /// u32 の値を u8 に変換(値が 0xff を超えるとエラー)
