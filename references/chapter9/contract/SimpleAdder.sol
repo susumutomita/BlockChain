500c4ad672b57603faea5e9dbe7c80f9b68b533c
susumutomita <oyster880@gmail.com>
2025-09-07 12:59:12 +0900

    add other caluculate

--- references/chapter9/contract/SimpleAdder.sol
+++ references/chapter9/contract/SimpleAdder.sol
@@ -5,4 +5,18 @@ contract Adder {
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
+
+    function sub(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b <= a, "underflow");
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) public pure returns (uint256) {
+        require(b != 0, "division by zero");
+        return a / b;
+    }
 }
